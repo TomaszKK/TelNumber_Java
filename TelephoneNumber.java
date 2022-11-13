89efbedcaeab89e4c72e0c04514ccdfff37cfdf5
TomaszKK <242136@edu.p.lodz.pl>
2022-11-13 14:35:14 +0100

    Add

---
+++ TelephoneNumber.java
@@ -0,0 +1,13 @@
+
+public class TelephoneNumber{
+    field countryCode, localNumber;
+    TelephoneNumber(field code, field Number){
+        countryCode = code;
+        localNumber = Number;
+    }
+    abstract class TelephoneEntry{
+        public TelephoneEntry() {
+        }
+
+    }
+}
---
+++ TestTelNumber.java
@@ -0,0 +1,5 @@
+public class TestTelNumber{
+    public static void main(String[] args){
+        
+    }
+}
