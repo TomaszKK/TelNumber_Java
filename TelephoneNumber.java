1a7f978de887dcc9ad8e44f8de4fd1a26df69023
TomaszKK <242136@edu.p.lodz.pl>
2022-11-13 20:31:29 +0100

    Add

--- TelephoneNumber.java
+++ TelephoneNumber.java
@@ -1,13 +1,39 @@
+import TelephoneNumber.TelephoneEntry;
 
-public class TelephoneNumber{
+public class TelephoneNumber implements Comparable<TelephoneNumber>{
     field countryCode, localNumber;
-    TelephoneNumber(field code, field Number){
+    public TelephoneNumber(field code, field Number){
         countryCode = code;
         localNumber = Number;
     }
+
+    public int compareTo(TelephoneNumber telephone){
+        if(this.countryCode == telephone.countryCode){
+            return 0;
+        }
+        else if(this.countryCode > telephone.countryCode){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+
     abstract class TelephoneEntry{
+
         public TelephoneEntry() {
         }
+        
+        abstract void desription();
 
     }
+}
+
+public class Persons extends TelephoneEntry{
+    String Name, LastName, Address;
+}
+
+public class Company extends TelephoneEntry{
+    String Name, Adress;
+  
 }
