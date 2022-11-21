b4434f0fe2cd2b28c82199170e74ee8cf858ae1e
TomaszKK <242136@edu.p.lodz.pl>
2022-11-21 18:17:00 +0100

    add

--- TestTelNumber.java
+++ TestTelNumber.java
@@ -4,8 +4,8 @@ public class TestTelNumber{
         Person person1 = new Person("Tyler", "Johnson", "12 South Street", "New York", "1", "154 887 8952");
         Person person2 = new Person("John", "Smith", "123 Main Street", "London", "44", "123 456 789");
         Person person3 = new Person("Kyle", "Brooks", "Kolorowa 21", "Warszawa", "48", "124 856 477");
-        Company company1 = new Company("Google", "1600 Amphitheatre Parkway", "Mountain View", "1", "555 555 1234");
-        Company company2 = new Company("Apple", "1 Apple Park Way", "Cupertino", "1", "408 996 1010");
+        Company company1 = new Company("FaceBook", "1600 Amphitheatre Parkway", "Mountain View", "1", "555 555 1234");
+        Company company2 = new Company("Twitter", "1 Apple Park Way", "Cupertino", "1", "408 996 1010");
         Company company3 = new Company("Microsoft", "Marszałkowska 26", "Warszawa", "48", "730114819");
        
         TreeMap<TelephoneNumber, TelephoneEntry> telephoneEntries = new TreeMap<TelephoneNumber, TelephoneEntry>();
@@ -15,7 +15,7 @@ public class TestTelNumber{
         telephoneEntries.put(company1.getTelephoneNumber(), company1);
         telephoneEntries.put(company2.getTelephoneNumber(), company2);
         telephoneEntries.put(company3.getTelephoneNumber(), company3);
-        
+
         Iterator<Map.Entry<TelephoneNumber, TelephoneEntry>> iterator = telephoneEntries.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<TelephoneNumber, TelephoneEntry> pair = iterator.next();
